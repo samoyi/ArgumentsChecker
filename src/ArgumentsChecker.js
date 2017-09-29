16fb72af04b8c27d4de6e961d42fda4e4ad4c9ed
samoyi <leening529@163.com>
2017-09-30 01:05:03 +0800

    正在整理

--- src/ArgumentsChecker.js
+++ src/ArgumentsChecker.js
@@ -3,108 +3,57 @@
 /*
  * 支持链式调用。但如果调用链某个方法报错后就不会执行后面的方法
  * 构造函数传参true或等值于true，则抛出错误是同时alert错误，便于在微信环境上查看错误
- *
- * TODO： 链式调用每个方法都需要输入函数名，麻烦
- *
- *
  */
 
-function ArgumentsChecker( bForWechat )
+function ArgumentsChecker( bAlert )
 {
-
-	const typeChecker = {
-		isObject(arg){
-			return typeof arg === 'object';
-		},
-		isPlainObjet(arg){
-			return arg instanceof Object;
-		},
-		isArray(arg){
-			return sArray(arg)
-		},
-		isIterator(arg){
-			return typeof arg['Symbol.iterator'] === 'function';
-		},
-		isNumber(arg){
-			return  arg && typeof arg === 'number';
-		},
-		isString(arg){
-			return typeof arg === 'string';
-		},
-		isBoolean(arg){
-			return typeof arg === 'boolean';
-		},
-		isDate(arg){
-			return arg instanceof Date;
-		}
-	};
-
 
 	// get type of checked with lower case
 	function getTypeWithLowerCase( checked ){
 		return Object.prototype.toString.call(checked).slice(8, -1).toLowerCase();
 	}
 
-	// Error
-	function throwErr( sErr ){
-		if( bForWechat ){
-			alert( sErr );
+	// Throw Error
+	const throwErr = ( sErr )=>{
+		// TODO 错误信息中的参数显示无法显示数字字符串的字符串形式
+		let sArgs = ' Arguments: [' + [...this.args].toString() + ']';
+		if( bAlert ){
+			alert( sErr + sArgs );
 		}
-		throw new Error( sErr );
+		throw new Error( sErr + sArgs );
 	}
 
-	if (typeof this.argumengsAmountCheck != 'function')
+	if (typeof this.amount != 'function')
 	{
+		//
+		ArgumentsChecker.prototype.get = (aArguments)=>{
+			this.args = aArguments;
+			return this;
+		};
+
 		// If arguments amount is less than nExpected, throw an error
-		ArgumentsChecker.prototype.amount = function(aArguments, nExpected){
-			let nArgumentsLength = aArguments.length;
+		ArgumentsChecker.prototype.amount = (nExpected)=>{
+			let nArgumentsLength = this.args.length;
 			if( nArgumentsLength < nExpected ){
-				throwErr( "ArgumentsChecker: Expects " + nExpected + " arguments, " + nArgumentsLength + " given" );
+				throwErr( "ArgumentsChecker: Expects at least " + nExpected + " arguments, " + nArgumentsLength + " given." );
 			}
 			return this;
 		};
 
 
-		// check if all of given arguments in aArguments is the type of sExpectedType, if not throw an error
-		/*
-		 * sExpectedType is case-insensitive
-		 */
-		ArgumentsChecker.prototype.checkArgumentsType = function(sFunctionName, aArguments, sExpectedType)
-		{
-
-			sExpectedType = sExpectedType.toLocaleLowerCase();
-			var sType = "";
-			Array.prototype.forEach.call(aArguments, function(item, index)
-			{
-				sType = getTypeWithLowerCase( item );
-				if( sType !== sExpectedType )
-				{
-					throwErr( "One of arguments in function " + sFunctionName + " has wrong type, expected " + sExpectedType + ", " + sType + " given" );
+		// Check arguments type
+		ArgumentsChecker.prototype.type = (aExpected)=>{
+			;"use strict";
+			aExpected.forEach((type, index)=>{
+				;"use strict";
+				if(type){
+					console.log(this); // TODO 如果不用箭头函数，这里为什么是 Window
+					let sGivenType =  getTypeWithLowerCase(this.args[index]);
+					if( type.toLowerCase() !== sGivenType ){
+						throwErr( "ArgumentsChecker: Expects " + type + ", " + sGivenType + " given." );
+					}
 				}
 			});
-			return this;
-		};
-
-		// check if an argument is plain object
-		/*
-		 * if bCouldUndefined is given and equals true, undefined in checkedArgument will not trigger an error
-		 */
-		ArgumentsChecker.prototype.confirmPlainObject = function (sFunctionName, checkedArgument, bCouldUndefined)
-		{
-			if( arguments.length < 2 )
-			{
-				throwErr( "ArgumentsChecker.confirmPlainObject needs 2 arguments at least" );
-			}
-			if( typeof sFunctionName !== "string" )
-			{
-				throwErr( "The first arguments of ArgumentsChecker.confirmPlainObject should be function name string" );
-			}
-
-			var sTypeGiven = getTypeWithLowerCase( checkedArgument );
-			if( (sTypeGiven !== "object" && sTypeGiven !== "undefined" ) || ( !bCouldUndefined && sTypeGiven === "undefined" ) )
-			{
-				throwErr( "One of arguments in function " + sFunctionName + " has a wrong type. Expected plain object, " + JSON.stringify(checkedArgument) + " given." );
-			}
 			return this;
 		}
 	}
